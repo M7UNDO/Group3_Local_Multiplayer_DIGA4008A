a5db72afe50b5b1ffa68c971dd4a1aaa496b25e2
RhettiBOi <vdhrhett@icloud.com>
2026-03-09 22:52:51 +0200

    script complete

--- Group3_Local Multiplayer Game/Assets/Scripts/Objective/Completion.cs
+++ Group3_Local Multiplayer Game/Assets/Scripts/Objective/Completion.cs
@@ -4,16 +4,26 @@ public class Completion : MonoBehaviour
 {
     public int Candies = 0;
     public GameObject Endscreen;
+    public GameObject EndBox;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Endscreen.SetActive(false);
+        EndBox.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Candies >= 2)
+        {
+           EndBox.SetActive(true);
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
             Endscreen.SetActive(true);
         }
